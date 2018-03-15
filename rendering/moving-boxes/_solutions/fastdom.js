bfe439325d44a435f09c5cd888f1d42a3634e7d9
Steve Kinney <hello@stevekinney.net>
2018-03-15 09:14:45 -0500

    Update code

--- rendering/moving-boxes/_solutions/fastdom.js
+++ rendering/moving-boxes/_solutions/fastdom.js
@@ -3,16 +3,16 @@ const {
   registerNextClick,
  } = setup();
 
- const elements = Array.from(document.querySelectorAll('.element'));
+const elements = Array.from(document.querySelectorAll('.element'));
 
- registerNextClick((timestamp) => {
-  elements.forEach((element) => {
-    fastdom.measure(() => {
-      const top = element.offsetTop;
-      const nextPosition = +((Math.sin(top + timestamp/1000) + 1) / 2 * containerWidth);
-      fastdom.mutate(() => {
-        element.style.transform = `translateX(${nextPosition}px)`;
-      });
+registerNextClick((timestamp) => {
+elements.forEach((element) => {
+  fastdom.measure(() => {
+    const top = element.offsetTop;
+    const nextPosition = +((Math.sin(top + timestamp/1000) + 1) / 2 * containerWidth);
+    fastdom.mutate(() => {
+      element.style.transform = `translateX(${nextPosition}px)`;
     });
   });
- });
+});
+});
